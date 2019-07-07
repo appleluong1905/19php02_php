32c8f75f16a77b9065c1688cc0dc9c2f32e66f2f
Chad Luong <apple.luong1905@gmail.com>
2019-07-07 15:44:53 +0700

    update

--- session18/add_news.php
+++ session18/add_news.php
@@ -10,14 +10,21 @@
 	if (isset($_POST['add_news'])) {
 		$title = $_POST['title'];
 		$description = $_POST['description'];
-		$sql = "INSERT INTO news(title, description) VALUES('$title', '$description')";
+		$image = 'default.png';
+		// upload image
+		if ($_FILES['image']['error'] == 0) {
+			$image = $_FILES['image']['name'];
+			move_uploaded_file($_FILES['image']['tmp_name'], 'uploads/'.$image);
+		}
+		$sql = "INSERT INTO news(title, description, image) VALUES('$title', '$description', '$image')";
+		//
 		if (mysqli_query($connect, $sql) === TRUE) {
 			header('Location: list_news.php');
 		}
 	}
 	?>
 	<h1>Add news</h1>
-	<form method="post" action="#">
+	<form method="post" action="#" enctype="multipart/form-data">
 		<p>
 			Title:
 			<input type="text" name="title">
